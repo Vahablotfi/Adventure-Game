686b118c278286642500e4f5b08966442b72b586
Vahablotfi <vahablotfi1@gmail.com>
2024-03-04 11:30:02 +0100

    room-class added

---
+++ src/Room.java
@@ -0,0 +1,56 @@
+public class Room {
+    private String name;
+    private String description;
+    private String north;
+    private String east;
+    private String west;
+    private String south;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNorth() {
+        return north;
+    }
+
+    public void setNorth(String north) {
+        this.north = north;
+    }
+
+    public String getEast() {
+        return east;
+    }
+
+    public void setEast(String east) {
+        this.east = east;
+    }
+
+    public String getWest() {
+        return west;
+    }
+
+    public void setWest(String west) {
+        this.west = west;
+    }
+
+    public String getSouth() {
+        return south;
+    }
+
+    public void setSouth(String south) {
+        this.south = south;
+    }
+}
